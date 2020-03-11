f4aec6908692ba654a21aa60a46e959574dfa5c0
inbary-s <inbarsim6@gmail.com>
2020-03-11 16:30:48 -0600

    initial commit, some backend wiring

---
+++ models/exercise.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+
+ exercises: {
+     type: Array,
+     default: []
+ },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Exercise = mongoose.model("exercise", exerciseSchema);
+
+module.exports = Exercise;
